c8a4a93e27e16188feb68c3b3bd992f4e735159c
Audrey Tang <audreyt@audreyt.org>
2013-11-27 18:01:34 +0800

    * CSLD 2013 Draft edition, programmatic proofreading, part 4.

--- q4.pl
+++ q4.pl
@@ -8,8 +8,6 @@ binmode STDOUT, ':utf8';
 my (%variants_id, %variants_ch);
 my $csld = decode_json(read_file('dict-csld.json', {binmode => ':mmap'}));
 
-goto Q2;
-
 for my $entry (@$csld) {
     for my $hetero (@{ $entry->{heteronyms} }) {
         next unless $hetero->{id} =~ /^4/;
@@ -40,11 +38,49 @@ Q2:
 open my $fh, '<:mmap', '兩岸常用詞典2013.csv';
 require Text::CSV_XS;
 my $csv = Text::CSV_XS->new ({ binary => 1 });
-
+<$fh>;
+my @solos;
+while (my $row = $csv->getline ($fh)) {
+    my (undef, undef, undef, $id, $title, undef, undef, undef, undef, undef, undef, undef, undef, @defs) = @$row;
+    @defs = grep { length } @defs;
 #(1) 哪些字詞的義項序有重號或跳號？
-
+    if (@defs > 1) {
+        my $ord = 1;
+        for my $def (@defs) {
+            next unless $def =~ /([1-9]\d*)\.(?![56])/;
+            say "$id\t$title\t$ord\t$1" unless $def =~ /$ord\.(?![56])/;
+            $ord++;
+        }
+    }
+    else {
 #(2) 哪些字詞僅有一個義項卻誤填了義項序？
+        push @solos, "$id\t$title\t@defs" if $defs[0] =~ /^\d+\./;
+    }
+}
+say $_ for @solos;
 
+
+Q3:
+use Regexp::Common qw /balanced/;
 #3. 成組符號是否有缺漏？
 #　　EX. 「」、『』、[]、〈〉、《》？
+my @opener = qw(（ 「 『 [ 〈 《);
+my @closer = qw(） 」 』 ] 〉 》);
 
+my $re = $RE{balanced}
+    {-begin => join '|', @opener}
+    {-end   => join '|', @closer};
+for my $entry (@$csld) {
+    for my $hetero (@{ $entry->{heteronyms} }) {
+        for my $def (@{ $hetero->{definitions} }) {
+            my $text = $def->{def};
+            for my $idx (0..$#opener) {
+                my $opener = $opener[$idx];
+                my $closer = $closer[$idx];
+                if ("$opener$text$closer" !~ /^$re$/o) {
+                    say "$hetero->{id}\t$entry->{title}\t$opener$closer\t$text";
+                }
+            }
+        }
+    }
+}
