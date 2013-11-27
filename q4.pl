3508a4c2df433e8499659e03b8ccd47edfc5b25c
Audrey Tang <audreyt@audreyt.org>
2013-11-27 18:47:27 +0800

    * Check balancing text from csv

--- q4.pl
+++ q4.pl
@@ -33,13 +33,25 @@ for my $entry (@$csld) {
 }
 
 Q2:
+Q3:
 #2. 義項序是否正確？
+#3. 成組符號是否有缺漏？
 
 open my $fh, '<:mmap', '兩岸常用詞典2013.csv';
 require Text::CSV_XS;
 my $csv = Text::CSV_XS->new ({ binary => 1 });
 <$fh>;
 my @solos;
+my @multi;
+my @imbas;
+
+use Regexp::Common qw /balanced/;
+my @opener = qw(（ 「 『 [ 〈 《);
+my @closer = qw(） 」 』 ] 〉 》);
+
+my $re = $RE{balanced}
+    {-begin => join '|', @opener}
+    {-end   => join '|', @closer};
 while (my $row = $csv->getline ($fh)) {
     my (undef, undef, undef, $id, $title, undef, undef, undef, undef, undef, undef, undef, undef, @defs) = @$row;
     @defs = grep { length } @defs;
@@ -48,7 +60,7 @@ while (my $row = $csv->getline ($fh)) {
         my $ord = 1;
         for my $def (@defs) {
             next unless $def =~ /([1-9]\d*)\.(?![56])/;
-            say "$id\t$title\t$ord\t$1" unless $def =~ /$ord\.(?![56])/;
+            push @multi, "$id\t$title\t$ord\t$1" unless $def =~ /$ord\.(?![56])/;
             $ord++;
         }
     }
@@ -56,31 +68,17 @@ while (my $row = $csv->getline ($fh)) {
 #(2) 哪些字詞僅有一個義項卻誤填了義項序？
         push @solos, "$id\t$title\t@defs" if $defs[0] =~ /^\d+\./;
     }
-}
-say $_ for @solos;
-
-
-Q3:
-use Regexp::Common qw /balanced/;
-#3. 成組符號是否有缺漏？
-#　　EX. 「」、『』、[]、〈〉、《》？
-my @opener = qw(（ 「 『 [ 〈 《);
-my @closer = qw(） 」 』 ] 〉 》);
-
-my $re = $RE{balanced}
-    {-begin => join '|', @opener}
-    {-end   => join '|', @closer};
-for my $entry (@$csld) {
-    for my $hetero (@{ $entry->{heteronyms} }) {
-        for my $def (@{ $hetero->{definitions} }) {
-            my $text = $def->{def};
-            for my $idx (0..$#opener) {
-                my $opener = $opener[$idx];
-                my $closer = $closer[$idx];
-                if ("$opener$text$closer" !~ /^$re$/o) {
-                    say "$hetero->{id}\t$entry->{title}\t$opener$closer\t$text";
-                }
+    for my $def (@defs) {
+        Encode::_utf8_on($def);
+        for my $idx (0..$#opener) {
+            my $opener = $opener[$idx];
+            my $closer = $closer[$idx];
+            if ("$opener$def$closer" !~ /^$re$/o) {
+                Encode::_utf8_on($title);
+                say "$id\t$title\t$opener$closer\t$def";
             }
         }
     }
 }
+say Encode::decode_utf8($_) for @multi;
+say Encode::decode_utf8($_) for @solos;
