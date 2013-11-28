8de121463a6d695deb1b04b16e13fec18cc33a9b
Audrey Tang <audreyt@audreyt.org>
2013-11-29 01:51:50 +0800

    * Split 1/2/3+ comparisons to workaorund 65k row limitation

--- q5.pl
+++ q5.pl
@@ -20,12 +20,14 @@ my $x = $csld_titles->asymmetric_difference($revised_titles);
 say join $/, @{ $x->[0] };
 
 #本詞典未收那些字詞？
-say join $/, @{ $x->[1] };
+say join $/, grep { length == 1 } @{ $x->[1] };
+say join $/, grep { length == 2 } @{ $x->[1] };
+say join $/, grep { length > 2 } @{ $x->[1] };
 
 Q2:
 #2. 本詞典與大陸版《兩岸常用詞典》比對：
 my $cn_titles = Set::SortedArray->new(
-    map { s/^,|^[（（][^,]+//r =~ s/[,\s].*//r } split(/\n/, read_file('大陸版字詞目及音讀.csv', {binmode => ':utf8'}))
+    map { s/^,|^[(（（][^,]+//r =~ s/[,\s].*//r } split(/\n/, read_file('大陸版字詞目及音讀.csv', {binmode => ':utf8'}))
 );
 
 #本詞典多收那些字詞？
