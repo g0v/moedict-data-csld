971b40680c2b20c5dd5f6dfadb88e275f932b6e9
Audrey Tang <audreyt@audreyt.org>
2013-11-29 21:07:39 +0800

    * q3-2

--- q3.pl
+++ q3.pl
@@ -8,8 +8,9 @@ binmode STDOUT, ':utf8';
 my (%variants_id, %variants_ch);
 my $csld = decode_json(read_file('dict-csld.json', {binmode => ':mmap'}));
 
-goto Q2;
+goto Q3;
 
+Q1:
 my %sound;
 for my $file (qw[ 國語一字多音審訂表1999.csv 國語一字多音審訂表2012.csv ]) {
     open my $fh, '<:utf8', $file; <$fh>;
@@ -74,10 +75,51 @@ for my $entry (@$csld) {
 #2. 哪些詞素取音不符合字頭收音？
 Q2:
 
+my %solo;
+for my $entry (@$csld) {
+    next unless length($entry->{title}) == 1;
+    for my $hetero (@{ $entry->{heteronyms} }) {
+        my $bpmf = $hetero->{bopomofo};
+        $bpmf =~ s/<br>.*//;
+        $bpmf =~ s/陸⃟//g;
+        $bpmf =~ s/臺⃟//g;
+        $bpmf =~ s/[臺陸]//g;
+        $bpmf =~ s/ㄧ/｜/g;
+        $solo{$entry->{title}}{$bpmf}++;
+    }
+}
+
+for my $entry (@$csld) {
+    next unless length($entry->{title}) > 1;
+    for my $hetero (@{ $entry->{heteronyms} }) {
+        my $bpmf = $hetero->{bopomofo};
+        $bpmf =~ s/<br>.*//;
+        $bpmf =~ s/陸⃟//g;
+        $bpmf =~ s/臺⃟//g;
+        $bpmf =~ s/[臺陸]//g;
+        $bpmf =~ s/ㄧ/｜/g;
+        my @bpmf = grep /\S/, split /[﹐,，\s]+/, $bpmf;
+        my $title = $entry->{title} =~ s/[﹐，,]//gr;
+        next if length $title != @bpmf and $title =~ /兒/; # 兒化韻
+        next if length $title != @bpmf and !@bpmf;
+        #say "$hetero->{id}\t$title\t$hetero->{bopomofo}" unless length $title == @bpmf;
+        next unless length $title == @bpmf;
+        for my $word (split //, $title) {
+            say "$hetero->{id}\t$title\t$word\t$bpmf[0]" unless $solo{$word}{$bpmf[0]} or $bpmf[0] =~ /[•˙˙‧]/ or $word =~ /[一不]/;
+            shift @bpmf;
+        }
+    }
+}
+
 #3. 哪些字詞的注音符號與漢語拼音的音讀不一致？
+Q3:
 
 #4. 哪些字詞的漢語拼音有誤？
+Q4:
 
 #5. 音序是否正確？
+Q5:
 #(1) 哪些多音字詞的音序有重號或跳號？
+
+Q5_2:
 #(2) 哪些單音字詞誤填了音序？
